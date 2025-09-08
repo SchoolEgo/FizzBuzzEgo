54490b0c54edf50b8a51e49d118eebfa34b0e045
Julian Sudac <julian.sudac@mail.utoronto.ca>
2025-09-08 12:18:17 -0400

    Fixed Multiples to actually print out the correct answer

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,18 +2,22 @@ package lab1;
 
 public class Multiples {
     public static void main(String[] args) {
-        int multOf3 = 0, multOf5 = 0;
+        int mults = 0;
 
-        for (int i = 1; i < 1000; i++){
-            if (i % 3 == 0){
-                multOf3++;
-            }
-            if (i % 5 == 0){
-                multOf5++;
+        mults = multiples();
+
+        System.out.println(mults);
+    }
+
+    private static int multiples() {
+        int count = 0;
+
+        for (int i = 1; i < 1000; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                count++;
             }
         }
 
-        System.out.println(multOf3);
-        System.out.println(multOf5);
+        return count;
     }
 }
